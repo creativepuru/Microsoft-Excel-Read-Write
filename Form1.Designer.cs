f87d187fac3e3a9ef497397a4a7b249cbf0e64c4
Purushottam Anand <creativepurus@gmail.com>
2023-08-12 07:45:18 +0530

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace ReadWriteExcel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonRead = new System.Windows.Forms.Button();
+            this.buttonWrite = new System.Windows.Forms.Button();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonRead
+            // 
+            this.buttonRead.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonRead.Location = new System.Drawing.Point(66, 359);
+            this.buttonRead.Name = "buttonRead";
+            this.buttonRead.Size = new System.Drawing.Size(78, 27);
+            this.buttonRead.TabIndex = 0;
+            this.buttonRead.Text = "Read";
+            this.buttonRead.UseVisualStyleBackColor = true;
+            this.buttonRead.Click += new System.EventHandler(this.buttonRead_Click);
+            // 
+            // buttonWrite
+            // 
+            this.buttonWrite.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonWrite.Location = new System.Drawing.Point(288, 359);
+            this.buttonWrite.Name = "buttonWrite";
+            this.buttonWrite.Size = new System.Drawing.Size(78, 27);
+            this.buttonWrite.TabIndex = 1;
+            this.buttonWrite.Text = "Write";
+            this.buttonWrite.UseVisualStyleBackColor = true;
+            this.buttonWrite.Click += new System.EventHandler(this.buttonWrite_Click);
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowHeadersWidth = 51;
+            this.dataGridView.RowTemplate.Height = 24;
+            this.dataGridView.Size = new System.Drawing.Size(498, 327);
+            this.dataGridView.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(531, 410);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.buttonWrite);
+            this.Controls.Add(this.buttonRead);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonRead;
+        private System.Windows.Forms.Button buttonWrite;
+        private System.Windows.Forms.DataGridView dataGridView;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace ReadWriteExcel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonRead_Click(object sender, EventArgs e)
+        {
+            Excel.Application excelApp = new Excel.Application();
+            string currentDirectory = Directory.GetCurrentDirectory();
+            string filePath = Path.Combine(currentDirectory, "TempReader.xlsx");
+
+            Excel.Workbook workbook = excelApp.Workbooks.Open(filePath);
+            Excel.Worksheet worksheet = workbook.Sheets[1]; // Assuming data is on the first sheet
+
+            int rowCount = worksheet.UsedRange.Rows.Count;
+            List<string> cellValues = new List<string>();
+
+            // Read data from all cells in the first column
+            for (int row = 1; row <= rowCount; row++)
+            {
+                Excel.Range cell = worksheet.Cells[row, 1];
+                string cellValue = cell.Value != null ? cell.Value.ToString() : "";
+                cellValues.Add(cellValue);
+            }
+
+            // Define a column for the DataGridView
+            dataGridView.Columns.Clear();
+            dataGridView.Columns.Add("CellValue", "Cell Value");
+
+            // Display all data in DataGridView
+            dataGridView.Rows.Clear();
+            dataGridView.Rows.AddRange(cellValues.Select(value => new DataGridViewRow { Cells = { new DataGridViewTextBoxCell { Value = value } } }).ToArray());
+
+            // Close workbook and Excel application
+            workbook.Close(false);
+            excelApp.Quit();
+
+        }
+
+        private void buttonWrite_Click(object sender, EventArgs e)
+        {
+            Excel.Application excelApp = new Excel.Application();
+            string currentDirectory = Directory.GetCurrentDirectory();
+            string filePath = Path.Combine(currentDirectory, "TempReader.xlsx");
+
+            Excel.Workbook workbook = excelApp.Workbooks.Open(filePath);
+            Excel.Worksheet worksheet = workbook.Sheets[1]; // Accessing the first sheet
+
+            worksheet.Cells[5, 1] = "E";
+
+            // Save the workbook
+            workbook.Save();
+
+            // Close workbook and Excel application
+            workbook.Close(false);
+            excelApp.Quit();
+
+            MessageBox.Show("Data written to Excel successfully.");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
